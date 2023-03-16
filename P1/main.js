e612355e05f050858f40523e78649c1afd7cfbfd
Vaibhav <vaibhavsinghvrajpot@gmail.com>
2023-03-16 08:46:53 +0530

    4th commit

--- P1/main.js
+++ P1/main.js
@@ -11,10 +11,10 @@ mainHeader.style.border = 'solid 3px #000'
 
 //second task
 
-let lists = document.getElementsByClassName('list-group-item')
-for(let list of lists)
-list.style.fontWeight = 'bold'
-lists[2].style.backgroundColor = 'green'
+// let lists = document.getElementsByClassName('list-group-item')
+// for(let list of lists)
+// list.style.fontWeight = 'bold'
+// lists[2].style.backgroundColor = 'green'
 
 // third task 
 let parent1 = document.getElementsByClassName('list-group')
@@ -24,5 +24,19 @@ let li = document.createElement('li')
 let msgNode = document.createTextNode('new item')
   li.appendChild(msgNode)
   pItems[3].appendChild(li)
+
+  // foth task querySelector
+  let list2 = document.querySelector('.list-group-item:nth-child(2)')
+  let list3 = document.querySelector('.list-group-item:nth-child(3)')
+  list2.style.backgroundColor = 'green'
+  list3.style.display = 'none'
+  
+  let lists2 = document.querySelectorAll('.list-group-item')
+  lists2[2].style.color = 'green'
+
+  let oddlists = document.querySelectorAll('li:nth-child(odd)')
+  for(let i=0;i<oddlists.length;i++)
+  oddlists[i].style.backgroundColor = 'green'
+
    
 
